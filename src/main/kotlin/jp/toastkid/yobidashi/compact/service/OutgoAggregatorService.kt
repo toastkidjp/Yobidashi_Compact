5cad85620af1b3c7f693a3a058501ad6b886ae82
Toast Kid <super_q1m@yahoo.co.jp>
2020-05-01 07:54:09 +0900

    refactor Extract aggregation to service.

---
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/service/OutgoAggregatorService.kt
@@ -0,0 +1,42 @@
+package jp.toastkid.yobidashi.compact.service
+
+import jp.toastkid.yobidashi.compact.model.Setting
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.stream.Collectors
+
+class OutgoAggregatorService {
+
+    operator fun invoke(keyword: String): Map<String, Int> {
+        val map = mutableMapOf<String, Int>()
+        Files.list(Paths.get(Setting.articleFolder()))
+                .parallel()
+                .map { it.fileName.toString() to Files.readAllLines(it) }
+                .filter { it.first.startsWith(keyword) }
+                .map {
+                    var isOutGoLine = false
+                    for (line in it.second) {
+                        if ((line.startsWith("#")) && line.endsWith("家計簿")) {
+                            isOutGoLine = true
+                        }
+
+                        if (!isOutGoLine || !line.startsWith("|")) {
+                            continue
+                        }
+
+                        val items = line.split("|")
+                        val target = items.get(2)
+                        var price = 0
+                        if (target.endsWith("円")) {
+                            val priceStr = target.substring(0, target.indexOf("円")).trim().replace(",", "")
+                            if (priceStr.isNotBlank()) {
+                                price = Integer.parseInt(priceStr)
+                            }
+                            map.put(items.get(0) +  items.get(1).trim(), price)
+                        }
+                    }
+                }
+                .collect(Collectors.toList())
+        return map
+    }
+}
