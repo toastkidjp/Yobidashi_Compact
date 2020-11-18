18a2b29f403d9ea2febd72be7744e860ec1f48a3
Toast Kid <super_q1m@yahoo.co.jp>
2020-11-19 07:39:11 +0900

    refactor Clean up code.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/editor/text/ListHeadAdder.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/editor/text/ListHeadAdder.kt
@@ -3,14 +3,8 @@ package jp.toastkid.yobidashi.compact.editor.text
 class ListHeadAdder {
 
     /**
-     * Line separator.
-     */
-    private val lineSeparator = System.getProperty("line.separator") ?: "\n"
-
-    /**
-     * Invoke quotation function.
-     *
      * @param str Nullable [CharSequence]
+     * @param head
      */
     operator fun invoke(text: String?, head: String): String? {
         if (text.isNullOrEmpty()) {
@@ -18,4 +12,5 @@ class ListHeadAdder {
         }
         return head + text.replace("\n", "\n $head")
     }
+
 }
