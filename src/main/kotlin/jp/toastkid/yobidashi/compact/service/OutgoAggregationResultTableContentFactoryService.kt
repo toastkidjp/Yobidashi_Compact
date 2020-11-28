38680fb3063a3b5c843359feadf68187735f3398
Toast Kid <super_q1m@yahoo.co.jp>
2020-11-29 07:50:45 +0900

    refactor Modify ScrollPane to JScrollPane.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/service/OutgoAggregationResultTableContentFactoryService.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/service/OutgoAggregationResultTableContentFactoryService.kt
@@ -3,16 +3,14 @@ package jp.toastkid.yobidashi.compact.service
 import jp.toastkid.yobidashi.compact.model.OutgoAggregationResult
 import java.awt.Dimension
 import java.awt.Font
-import java.awt.ScrollPane
-import javax.swing.BoxLayout
-import javax.swing.JPanel
+import javax.swing.JComponent
+import javax.swing.JScrollPane
 import javax.swing.JTable
 import javax.swing.table.DefaultTableModel
 
-
 class OutgoAggregationResultTableContentFactoryService {
 
-    operator fun invoke(aggregationResult: OutgoAggregationResult): ScrollPane {
+    operator fun invoke(aggregationResult: OutgoAggregationResult): JComponent {
         val tableModel = object : DefaultTableModel(arrayOf("Date", "Item", "Price"), 0) {
             override fun getColumnClass(columnIndex: Int): Class<*> {
                 return when (columnIndex) {
@@ -30,14 +28,7 @@ class OutgoAggregationResultTableContentFactoryService {
         table.setAutoCreateRowSorter(true)
 
         aggregationResult.makeItemArrays().forEach { tableModel.addRow(it) }
-        val scrollPane = ScrollPane().also {
-            it.add(
-                    JPanel().also {
-                        it.layout = BoxLayout(it, BoxLayout.PAGE_AXIS)
-                        it.add(table.tableHeader)
-                        it.add(table)
-                    }
-            )
+        val scrollPane = JScrollPane(table).also {
             it.preferredSize = Dimension(600, 400)
         }
 
