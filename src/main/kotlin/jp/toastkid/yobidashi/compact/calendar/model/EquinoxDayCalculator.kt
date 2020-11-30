e25782866e70758d97dfeb08c1655f582e7d6f8b
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-01 07:18:05 +0900

    feature Implement EquinoxDayCalculator.

---
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/calendar/model/EquinoxDayCalculator.kt
@@ -0,0 +1,13 @@
+package jp.toastkid.yobidashi.compact.calendar.model
+
+class EquinoxDayCalculator {
+
+    fun calculateVernalEquinoxDay(year: Int): Int {
+        return (20.8431 + 0.242194 * (year - 1980)).toInt() - ((year - 1980) / 4).toInt()
+    }
+
+    fun calculateAutumnalEquinoxDay(year: Int): Int {
+        return (23.2488 + 0.242194 * (year - 1980)).toInt() - ((year - 1980) / 4).toInt()
+    }
+
+}
