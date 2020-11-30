6cb57b11029d2404af7314b3e0a4f287ee50d7bd
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-01 07:18:30 +0900

    feature Use EquinoxDayCalculator.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/calendar/usecase/OffDayFinderUseCase.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/calendar/usecase/OffDayFinderUseCase.kt
@@ -1,13 +1,24 @@
 package jp.toastkid.yobidashi.compact.calendar.usecase
 
+import jp.toastkid.yobidashi.compact.calendar.model.EquinoxDayCalculator
 import jp.toastkid.yobidashi.compact.calendar.model.FixedJapaneseHoliday
 import java.awt.Color
 
 class OffDayFinderUseCase {
 
+    private val equinoxDayCalculator = EquinoxDayCalculator()
+
     operator fun invoke(year: Int, month: Int, date: Int): Color {
         if (month == 6) {
             return Color.BLACK
+        }
+
+        if (month == 3 && date == equinoxDayCalculator.calculateVernalEquinoxDay(year)) {
+            return Color.RED
+        }
+
+        if (month == 9 && date == equinoxDayCalculator.calculateAutumnalEquinoxDay(year)) {
+            return Color.RED
         }
 
         return if (FixedJapaneseHoliday.values()
