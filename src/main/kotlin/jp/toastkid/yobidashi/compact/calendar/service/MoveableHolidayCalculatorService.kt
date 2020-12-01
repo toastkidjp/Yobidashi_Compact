130b6e5f54d519bf919218a020a6e81f7e2ac889
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-02 07:38:37 +0900

    feature Add 2020 and 2021 cases.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/calendar/service/MoveableHolidayCalculatorService.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/calendar/service/MoveableHolidayCalculatorService.kt
@@ -7,11 +7,46 @@ import java.util.GregorianCalendar
 class MoveableHolidayCalculatorService {
 
     operator fun invoke(year: Int, month: Int, date: Int): Boolean {
-        if (MoveableJapaneseHoliday.isTargetMonth(month).not()) {
+        if (MoveableJapaneseHoliday.isTargetMonth(month).not() && month != 8) {
             return false
         }
 
         if (year == 2019 && month == 5 && date == 1) {
+            return true
+        }
+
+        if (year == 2020) {
+            if (month == 7) {
+                return when (date) {
+                    23, 24 -> return true
+                    else -> false
+                }
+            }
+            if (month == 8) {
+                return date == 10
+            }
+            if (month == 10) {
+                return false
+            }
+        }
+
+        if (year == 2021) {
+            if (month == 7) {
+                return when (date) {
+                    22, 23 -> return true
+                    else -> false
+                }
+            }
+            if (month == 8) {
+                println("$year $month $date ${date == 9}")
+                return date == 9
+            }
+            if (month == 10) {
+                return false
+            }
+        }
+
+        if (month == 8 && date == 11) {
             return true
         }
 
