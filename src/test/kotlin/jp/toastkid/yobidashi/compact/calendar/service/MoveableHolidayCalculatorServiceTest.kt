00217b04395cc3c7c266cd325f5f774c44420e1b
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-02 07:21:41 +0900

    refactor Move packages.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/calendar/service/MoveableHolidayCalculatorServiceTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/calendar/service/MoveableHolidayCalculatorServiceTest.kt
@@ -1,4 +1,4 @@
-package jp.toastkid.yobidashi.compact.calendar.usecase
+package jp.toastkid.yobidashi.compact.calendar.service
 
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
