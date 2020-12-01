17133683b2afaea1a835bcfbcf700a076a94091e
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-02 07:17:20 +0900

    feature Implement MoveableHolidayCalculatorService.

---
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/calendar/usecase/MoveableHolidayCalculatorService.kt
@@ -0,0 +1,30 @@
+package jp.toastkid.yobidashi.compact.calendar.usecase
+
+import jp.toastkid.yobidashi.compact.calendar.model.MoveableJapaneseHoliday
+import java.util.Calendar
+import java.util.GregorianCalendar
+
+class MoveableHolidayCalculatorService {
+
+    operator fun invoke(year: Int, month: Int, date: Int): Boolean {
+        if (MoveableJapaneseHoliday.isTargetMonth(month).not()) {
+            return false
+        }
+
+        if (year == 2019 && month == 5 && date == 1) {
+            return true
+        }
+
+        val calendar = GregorianCalendar(year, month - 1, 1)
+        val targetDay = MoveableJapaneseHoliday.find(month) ?: return false
+        val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+        val d = if (dayOfWeek == Calendar.MONDAY) {
+            1
+        } else {
+            8 - (dayOfWeek - 2)
+        }
+
+        return date == d + (7 * (targetDay.week - 1))
+    }
+
+}
