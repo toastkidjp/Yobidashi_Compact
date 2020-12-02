007e6db079f52dc56a2c97994f066125000b8573
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-03 07:00:42 +0900

    feature Add accelerator for calendar menu.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/view/ToolMenuView.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/view/ToolMenuView.kt
@@ -1,9 +1,12 @@
 package jp.toastkid.yobidashi.compact.view
 
 import jp.toastkid.yobidashi.compact.calendar.view.CalendarPanel
+import java.awt.event.InputEvent
+import java.awt.event.KeyEvent
 import javax.swing.JMenu
 import javax.swing.JMenuItem
 import javax.swing.JOptionPane
+import javax.swing.KeyStroke
 
 class ToolMenuView {
 
@@ -13,6 +16,7 @@ class ToolMenuView {
             it.addActionListener {
                 JOptionPane.showMessageDialog(null, CalendarPanel())
             }
+            it.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_MASK)
         })
         return menu
     }
