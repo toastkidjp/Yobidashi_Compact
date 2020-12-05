867f54496fba70674fc80db8fbb8c2776ab3c660
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-06 08:52:35 +0900

    refactor Make ArticleLengthAggregationResult.

---
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/model/ArticleLengthAggregationResult.kt
@@ -0,0 +1,19 @@
+package jp.toastkid.yobidashi.compact.model
+
+class ArticleLengthAggregationResult : AggregationResult {
+
+    private val map: MutableMap<String, Long> = hashMapOf()
+
+    fun put(key: String, value: Long) {
+        map.put(key, value)
+    }
+
+    fun sum() = map.values.sum()
+
+    override fun header(): Array<Any> =
+            arrayOf("Title", "Length")
+
+    override fun itemArrays(): Collection<Array<Any>> =
+            map.entries.map { arrayOf(it.key, it.value) }
+
+}
