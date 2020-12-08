c7e4e321c0272818bd2056c50bba050c41655aee
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-09 07:30:00 +0900

    refactor Implement UrlOpenerService.

---
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/service/UrlOpenerService.kt
@@ -0,0 +1,15 @@
+package jp.toastkid.yobidashi.compact.service
+
+import java.awt.Desktop
+import java.net.URI
+
+class UrlOpenerService(private val desktop: Desktop = Desktop.getDesktop()) {
+
+    operator fun invoke(url: String?) {
+        if (url.isNullOrEmpty()) {
+            return
+        }
+
+        desktop.browse(URI(url))
+    }
+}
