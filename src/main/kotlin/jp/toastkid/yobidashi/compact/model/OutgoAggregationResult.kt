1dad2c56dbed5cbf36418097096ddaac8cd33c4a
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-10 06:34:33 +0900

    refactor Remove useless function.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/model/OutgoAggregationResult.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/model/OutgoAggregationResult.kt
@@ -12,10 +12,6 @@ class OutgoAggregationResult(val target: String): AggregationResult {
         return map.map { it.price }.sum()
     }
 
-    private fun detail(): String {
-        return map.map { "${it.date} ${it.title}: ${it.price}" }.reduce { base, item -> "$base${LINE_SEPARATOR}$item" }
-    }
-
     override fun itemArrays() = map.map { arrayOf(it.date, it.title, it.price) }
 
     override fun header(): Array<Any> = arrayOf("Date", "Item", "Price")
