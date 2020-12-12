4400d99118d7b93c8d06b6e1488b9808aa779476
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-12 10:38:55 +0900

    refactor Extract clipboard to field.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/editor/service/ClipboardFetcher.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/editor/service/ClipboardFetcher.kt
@@ -1,14 +1,13 @@
 package jp.toastkid.yobidashi.compact.editor.service
 
 import java.awt.Toolkit
+import java.awt.datatransfer.Clipboard
 import java.awt.datatransfer.DataFlavor
 
-class ClipboardFetcher {
+class ClipboardFetcher(private val clipboard: Clipboard = Toolkit.getDefaultToolkit().systemClipboard) {
 
     operator fun invoke(): String? {
-        val transferData = Toolkit.getDefaultToolkit()
-                .systemClipboard
-                .getContents(this)
+        val transferData = clipboard.getContents(this)
         return if (transferData?.isDataFlavorSupported(DataFlavor.stringFlavor) == false) null
         else transferData.getTransferData(DataFlavor.stringFlavor).toString()
     }
