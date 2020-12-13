4dd31cbf5112d6e632f2a7e339703d1adac06d0a
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-14 00:22:57 +0900

    refactor Use LocalDate.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/calendar/model/Month.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/calendar/model/Month.kt
@@ -1,18 +1,18 @@
 package jp.toastkid.yobidashi.compact.calendar.model
 
-import java.util.Calendar
+import java.time.LocalDate
 
 enum class Month {
     JAN, FEB, MAR, APR, MAY, JUNE, JULY, AUG, SEP, OCT, NOV, DEC;
 
-    fun isSameMonth(calendar: Calendar): Boolean {
-        return calendar.get(Calendar.MONTH) == this.ordinal
+    fun isSameMonth(date: LocalDate): Boolean {
+        return date.monthValue - 1 == this.ordinal
     }
 
     companion object {
 
-        fun from(calendar: Calendar): Month {
-            return values()[calendar.get(Calendar.MONTH)]
+        fun from(date: LocalDate): Month {
+            return values()[date.monthValue - 1]
         }
 
         fun fromName(name: String): Month {
