a2884ee47626535ac95a8d473f65f3563cb30a4e
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-19 09:16:12 +0900

    feature Override columnClass.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/aggregation/model/ArticleLengthAggregationResult.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/aggregation/model/ArticleLengthAggregationResult.kt
@@ -20,6 +20,13 @@ class ArticleLengthAggregationResult : AggregationResult {
         return "Total: ${sum()}"
     }
 
+    override fun columnClass(columnIndex: Int): Class<out Any> {
+        return when (columnIndex) {
+            1 -> Integer::class.java
+            else -> String::class.java
+        }
+    }
+
     override fun isEmpty(): Boolean {
         return map.isEmpty()
     }
