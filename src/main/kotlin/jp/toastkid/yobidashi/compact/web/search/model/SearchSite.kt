f9e88f84b128ee5047f3a3b1df6a08dd9aa60a7e
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-20 10:01:59 +0900

    feature Add shopping search.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/web/search/model/SearchSite.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/web/search/model/SearchSite.kt
@@ -6,7 +6,8 @@ import java.nio.charset.StandardCharsets
 
 enum class SearchSite(val siteName: String, private val searchUrlBase: String) {
     YAHOO_JAPAN("Yahoo! JAPAN", "https://search.yahoo.co.jp/search?p="),
-    WIKIPEDIA("Wikipedia", "https://ja.wikipedia.org/w/index.php?search=");
+    WIKIPEDIA("Wikipedia", "https://ja.wikipedia.org/w/index.php?search="),
+    AMAZON("Amazon", "https://www.amazon.co.jp/s?k=");
 
     fun make(rawQuery: String): URI {
         return URI("$searchUrlBase${URLEncoder.encode(rawQuery, StandardCharsets.UTF_8.name())}")
