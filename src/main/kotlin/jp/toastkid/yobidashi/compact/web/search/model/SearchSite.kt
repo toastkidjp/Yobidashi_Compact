5fd0f051e5e1cdd0691a573c4ef520e3eafb8639
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-21 07:18:31 +0900

    feature Add image search site.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/web/search/model/SearchSite.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/web/search/model/SearchSite.kt
@@ -7,7 +7,9 @@ import java.nio.charset.StandardCharsets
 enum class SearchSite(val siteName: String, private val searchUrlBase: String) {
     YAHOO_JAPAN("Yahoo! JAPAN", "https://search.yahoo.co.jp/search?p="),
     WIKIPEDIA("Wikipedia", "https://ja.wikipedia.org/w/index.php?search="),
-    AMAZON("Amazon", "https://www.amazon.co.jp/s?k=");
+    AMAZON("Amazon", "https://www.amazon.co.jp/s?k="),
+    IMAGE_YAHOO_JAPAN("Image (Yahoo! JAPAN)", "https://search.yahoo.co.jp/image/search?p=")
+    ;
 
     fun make(rawQuery: String): URI {
         return URI("$searchUrlBase${URLEncoder.encode(rawQuery, StandardCharsets.UTF_8.name())}")
