0bf5347a14583967f217bfeef9ac9e1ccdacab24
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-21 07:19:49 +0900

    refactor Extract factories to constructor's parameter for writeable unit test.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/web/search/view/SearchSiteSelectorFactory.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/web/search/view/SearchSiteSelectorFactory.kt
@@ -3,14 +3,20 @@ package jp.toastkid.yobidashi.compact.web.search.view
 import jp.toastkid.yobidashi.compact.web.search.model.SearchSite
 import jp.toastkid.yobidashi.compact.web.search.model.SearchSiteSelectorModel
 import java.awt.Dimension
+import javax.swing.ComboBoxModel
 import javax.swing.JComboBox
+import javax.swing.ListCellRenderer
 
-class SearchSiteSelectorFactory {
+class SearchSiteSelectorFactory(
+        private val valuesFactory: () -> Array<SearchSite> = { SearchSite.values() },
+        private val modelFactory: () -> ComboBoxModel<SearchSite> = { SearchSiteSelectorModel() },
+        private val rendererFactory: () -> ListCellRenderer<SearchSite> = { SearchSiteCellRenderer() }
+) {
 
     operator fun invoke(): JComboBox<SearchSite> {
-        return JComboBox(SearchSite.values()).also {
-            it.model = SearchSiteSelectorModel()
-            it.renderer = SearchSiteCellRenderer()
+        return JComboBox(valuesFactory()).also {
+            it.model = modelFactory()
+            it.renderer = rendererFactory()
             it.preferredSize = Dimension(50, 24)
         }
     }
