01991aa2f6103d8859ccfe968f2304a7fdd8fae8
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-23 08:06:44 +0900

    feature Implement AppearanceMenuView.

---
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/editor/view/AppearanceMenuView.kt
@@ -0,0 +1,27 @@
+package jp.toastkid.yobidashi.compact.editor.view
+
+import jp.toastkid.yobidashi.compact.editor.MenuCommand
+import jp.toastkid.yobidashi.compact.editor.service.ColorSettingService
+import kotlinx.coroutines.channels.Channel
+import javax.swing.JMenu
+import javax.swing.JMenuItem
+
+class AppearanceMenuView(private val channel: Channel<MenuCommand>) {
+
+    operator fun invoke(): JMenu {
+        val menu = JMenu("Appearance(A)")
+        menu.setMnemonic('A')
+
+        val findItem = JMenuItem("Color")
+        findItem.addActionListener {
+            ColorSettingService().invoke()
+            /*CoroutineScope(Dispatchers.Default).launch {
+                channel.send(MenuCommand.FIND)
+            }*/
+        }
+        menu.add(findItem)
+
+        return menu
+    }
+
+}
