9e78f5543986412644abdae9de458f9eb38587cf
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-23 09:06:41 +0900

    feature Implement color code decoder.

---
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/service/ColorDecoderService.kt
@@ -0,0 +1,16 @@
+package jp.toastkid.yobidashi.compact.service
+
+import java.awt.Color
+
+class ColorDecoderService {
+
+    operator fun invoke(argbColorCode: String?): Color? {
+        if (argbColorCode.isNullOrBlank()) {
+            return null
+        }
+
+        val code = if (argbColorCode.startsWith("#")) argbColorCode else "#$argbColorCode"
+        return Color(java.lang.Long.decode(code).toInt())
+    }
+
+}
