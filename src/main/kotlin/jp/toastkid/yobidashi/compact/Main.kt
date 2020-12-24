6f9c65409a5f724fdebc3028c5ecb6204906e5ae
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-25 06:50:12 +0900

    refactor Modify comment.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/Main.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/Main.kt
@@ -5,6 +5,7 @@ import jp.toastkid.yobidashi.compact.view.MainFrame
 
 /**
  * Created by toastkidjp on 2020/03/23.
+ * @author toastkidjp
  */
 fun main() {
     val frame = MainFrame("Yobidashi Compact")
