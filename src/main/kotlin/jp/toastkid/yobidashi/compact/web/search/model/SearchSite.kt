f86740bd2f2aff18643b47caf697a206cf95fc74
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-28 07:17:56 +0900

    feature Add realtime search.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/web/search/model/SearchSite.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/web/search/model/SearchSite.kt
@@ -8,7 +8,8 @@ enum class SearchSite(val siteName: String, private val searchUrlBase: String) {
     YAHOO_JAPAN("Yahoo! JAPAN", "https://search.yahoo.co.jp/search?p="),
     WIKIPEDIA("Wikipedia", "https://ja.wikipedia.org/w/index.php?search="),
     AMAZON("Amazon", "https://www.amazon.co.jp/s?k="),
-    IMAGE_YAHOO_JAPAN("Image (Yahoo! JAPAN)", "https://search.yahoo.co.jp/image/search?p=")
+    IMAGE_YAHOO_JAPAN("Image (Yahoo! JAPAN)", "https://search.yahoo.co.jp/image/search?p="),
+    REALTIME_YAHOO_JAPAN("Realtime (Yahoo! JAPAN)", "https://search.yahoo.co.jp/realtime/search?p=")
     ;
 
     fun make(rawQuery: String): URI {
