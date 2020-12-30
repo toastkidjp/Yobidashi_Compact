c111484d07f84aa8d7a25bb4aaa989cbe47942f8
Toast Kid <super_q1m@yahoo.co.jp>
2020-12-31 07:56:16 +0900

    refactor Implement WebSearchService's unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/web/search/WebSearchServiceTest.kt
@@ -0,0 +1,48 @@
+package jp.toastkid.yobidashi.compact.web.search
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.yobidashi.compact.service.UrlOpenerService
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.net.URI
+import javax.swing.JOptionPane
+
+internal class WebSearchServiceTest {
+
+    private lateinit var webSearchService: WebSearchService
+
+    @MockK
+    private lateinit var urlOpenerService: UrlOpenerService
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        webSearchService = WebSearchService(urlOpenerService)
+
+        mockkStatic(JOptionPane::class)
+        every { JOptionPane.showInputDialog(any()) }.answers { "test" }
+
+        every { urlOpenerService.invoke(any<URI>()) }.answers { Unit }
+    }
+
+    @Test
+    fun test() {
+        webSearchService()
+
+        verify(exactly = 1) { JOptionPane.showInputDialog(any()) }
+        verify(exactly = 1) { urlOpenerService.invoke(any<URI>()) }
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+}
