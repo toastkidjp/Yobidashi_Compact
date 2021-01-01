2253549ae38819b51a4fbe005825ad55eb87c301
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-02 00:03:08 +0900

    feature Implement model for calculate compound interest.

---
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/aggregation/model/CompoundInterestCalculationResult.kt
@@ -0,0 +1,31 @@
+package jp.toastkid.yobidashi.compact.aggregation.model
+
+class CompoundInterestCalculationResult : AggregationResult {
+
+    private val items = mutableListOf<Triple<Int, Int, Int>>()
+
+    override fun header(): Array<Any> {
+        return arrayOf("Year", "Single", "Compound")
+    }
+
+    override fun itemArrays(): Collection<Array<Any>> {
+        return items.map { arrayOf<Any>(it.first, it.second, it.third) }
+    }
+
+    override fun resultTitleSuffix(): String {
+        return " compound interests"
+    }
+
+    override fun isEmpty(): Boolean {
+        return items.isEmpty()
+    }
+
+    fun get(year: Int): Triple<Int, Int, Int>? {
+        return items.firstOrNull { it.first == year }
+    }
+
+    fun put(year: Int, singleCase: Int, compoundCase: Int) {
+        items.add(Triple(year, singleCase, compoundCase))
+    }
+
+}
