9645195cee8eb870c42711b3f25400005d80b7d2
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-03 08:02:32 +0900

    refactor Add test2020_Oct.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/calendar/service/SpecialCaseOffDayCalculatorServiceTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/calendar/service/SpecialCaseOffDayCalculatorServiceTest.kt
@@ -70,4 +70,12 @@ internal class SpecialCaseOffDayCalculatorServiceTest {
         assertTrue(forceNormal)
     }
 
+    @Test
+    fun test2020_Oct() {
+        val (isOffDay, forceNormal) = specialCaseOffDayCalculatorService.invoke(2020, 10, 11)
+
+        assertFalse(isOffDay)
+        assertTrue(forceNormal)
+    }
+
 }
