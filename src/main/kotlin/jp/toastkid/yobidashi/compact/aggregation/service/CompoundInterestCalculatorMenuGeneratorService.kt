32e436c2a71a1b2403623f5ad3a3eeb1d8db55d0
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-05 07:25:17 +0900

    refactor Extract action to function.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/aggregation/service/CompoundInterestCalculatorMenuGeneratorService.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/aggregation/service/CompoundInterestCalculatorMenuGeneratorService.kt
@@ -18,29 +18,33 @@ class CompoundInterestCalculatorMenuGeneratorService(
     operator fun invoke(): JMenuItem {
         val item = JMenuItem("Compound interest calculation")
         item.addActionListener {
-            val (installment, annualInterest, year) = inputService.invoke() ?: return@addActionListener
-
-            CoroutineScope(Dispatchers.Swing).launch {
-                try {
-                    val result = withContext(Dispatchers.IO) {
-                        calculatorService(installment, (annualInterest * 0.01), year)
-                    }
-
-                    if (result.isEmpty()) {
-                        JOptionPane.showConfirmDialog(null, "Result is empty.")
-                        return@launch
-                    }
-
-                    val table = aggregationResultTableFactoryService.invoke(result)
-                    // "Compound interest calculation. Installment = $installment, Annual interest = $annualInterest, Year = $year"
-                    SubjectPool.addNewTab(table, "$installment, $annualInterest, $year")
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    JOptionPane.showConfirmDialog(null, e)
-                }
-            }
+            onAction()
         }
         return item
+    }
+
+    private fun onAction() {
+        val (installment, annualInterest, year) = inputService.invoke() ?: return
+
+        CoroutineScope(Dispatchers.Swing).launch {
+            try {
+                val result = withContext(Dispatchers.IO) {
+                    calculatorService(installment, (annualInterest * 0.01), year)
+                }
+
+                if (result.isEmpty()) {
+                    JOptionPane.showConfirmDialog(null, "Result is empty.")
+                    return@launch
+                }
+
+                val table = aggregationResultTableFactoryService.invoke(result)
+                // "Compound interest calculation. Installment = $installment, Annual interest = $annualInterest, Year = $year"
+                SubjectPool.addNewTab(table, "$installment, $annualInterest, $year")
+            } catch (e: Exception) {
+                e.printStackTrace()
+                JOptionPane.showConfirmDialog(null, e)
+            }
+        }
     }
 
 }
