ba3268c8bbc6c5557fb363d461e5e2e47c9c3034
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-06 07:17:36 +0900

    refactor Implement TrimmingService's unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/text/TrimmingServiceTest.kt
@@ -0,0 +1,26 @@
+package jp.toastkid.yobidashi.compact.editor.text
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class TrimmingServiceTest {
+
+    private lateinit var trimmingService: TrimmingService
+
+    @BeforeEach
+    fun setUp() {
+        trimmingService = TrimmingService()
+    }
+
+    @Test
+    fun test() {
+        assertEquals("aaa", trimmingService.invoke("  aaa   "))
+        val lineSeparator = System.lineSeparator()
+        assertEquals(
+                "john${lineSeparator}aaa${lineSeparator}trimmed",
+                trimmingService.invoke(listOf("  john", " aaa   ", "trimmed  ").joinToString(lineSeparator))
+        )
+    }
+
+}
