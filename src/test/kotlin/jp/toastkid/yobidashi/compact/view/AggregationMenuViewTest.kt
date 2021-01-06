ca7cf032e6c6ad083bf85b967bcd8ade52535c5c
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-07 07:32:42 +0900

    refactor Adapt new constructor parameter.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/view/AggregationMenuViewTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/view/AggregationMenuViewTest.kt
@@ -8,6 +8,7 @@ import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
 import jp.toastkid.yobidashi.compact.aggregation.service.AggregationMenuItemGeneratorService
+import jp.toastkid.yobidashi.compact.aggregation.service.CompoundInterestCalculatorMenuGeneratorService
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -21,13 +22,17 @@ internal class AggregationMenuViewTest {
     @MockK
     private lateinit var aggregationMenuItemGeneratorService: AggregationMenuItemGeneratorService
 
+    @MockK
+    private lateinit var compoundInterestCalculatorMenuGeneratorService: CompoundInterestCalculatorMenuGeneratorService
+
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
 
-        aggregationMenuView = AggregationMenuView(aggregationMenuItemGeneratorService)
+        aggregationMenuView = AggregationMenuView(aggregationMenuItemGeneratorService, compoundInterestCalculatorMenuGeneratorService)
 
         every { aggregationMenuItemGeneratorService.invoke(any(), any(), any(), any()) }.answers { mockk() }
+        every { compoundInterestCalculatorMenuGeneratorService.invoke() }.answers { mockk() }
 
         mockkConstructor(JMenu::class)
         every { anyConstructed<JMenu>().add(any<JMenuItem>()) }.answers { mockk() }
@@ -38,6 +43,8 @@ internal class AggregationMenuViewTest {
         aggregationMenuView.invoke()
 
         verify(atLeast = 1) { anyConstructed<JMenu>().add(any<JMenuItem>()) }
+        verify(atLeast = 1) { aggregationMenuItemGeneratorService.invoke(any(), any(), any(), any()) }
+        verify(exactly = 1) { compoundInterestCalculatorMenuGeneratorService.invoke() }
     }
 
     @AfterEach
