301d6e5a58b65ae8ab1df02c8220e798309a3c8e
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-09 00:04:36 +0900

    refactor Add ArticleCellRenderer's unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/view/ArticleCellRendererTest.kt
@@ -0,0 +1,77 @@
+package jp.toastkid.yobidashi.compact.view
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.yobidashi.compact.model.Article
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.awt.Color
+import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.JList
+import javax.swing.JPanel
+
+internal class ArticleCellRendererTest {
+
+    private lateinit var articleCellRenderer: ArticleCellRenderer
+
+    @MockK
+    private lateinit var list: JList<out Article>
+
+    @MockK
+    private lateinit var value: Article
+
+    @BeforeEach
+    fun setUp() {
+        articleCellRenderer = ArticleCellRenderer()
+
+        MockKAnnotations.init(this)
+
+        every { list.getSelectionBackground() }.answers { Color.BLUE }
+        every { list.getSelectionForeground() }.answers { Color.WHITE }
+
+        every { value.getTitle() }.returns("Test")
+        every { value.lastModified() }.returns(1610059992L)
+
+        mockkConstructor(JPanel::class)
+        every { anyConstructed<JPanel>().setLayout(any()) }.answers { mockk() }
+        every { anyConstructed<JPanel>().add(any<JComponent>(), any()) }.answers { mockk() }
+        every { anyConstructed<JPanel>().setBackground(any()) }.answers { mockk() }
+        every { anyConstructed<JPanel>().setForeground(any()) }.answers { mockk() }
+        every { anyConstructed<JPanel>().setBorder(any()) }.answers { mockk() }
+
+        mockkConstructor(JLabel::class)
+        every { anyConstructed<JLabel>().setText(any()) }.answers { mockk() }
+        every { anyConstructed<JLabel>().setForeground(any()) }.answers { mockk() }
+    }
+
+    @Test
+    fun getListCellRendererComponent() {
+        articleCellRenderer.getListCellRendererComponent(
+                list, value, 1, true, true
+        )
+
+        verify(atLeast = 1) { list.getSelectionBackground() }
+        verify(atLeast = 1) { list.getSelectionForeground() }
+        verify(atLeast = 1) { value.getTitle() }
+        verify(atLeast = 1) { value.lastModified() }
+        verify(atLeast = 1) { anyConstructed<JPanel>().setLayout(any()) }
+        verify(atLeast = 1) { anyConstructed<JPanel>().setBackground(any()) }
+        verify(atLeast = 1) { anyConstructed<JPanel>().setForeground(any()) }
+        verify(atLeast = 1) { anyConstructed<JPanel>().setBorder(any()) }
+        verify(atLeast = 1) { anyConstructed<JLabel>().setText(any()) }
+        verify(atLeast = 1) { anyConstructed<JLabel>().setForeground(any()) }
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+}
