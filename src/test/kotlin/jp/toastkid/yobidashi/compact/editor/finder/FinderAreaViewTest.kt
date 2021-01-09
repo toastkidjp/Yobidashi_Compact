7596b66f35eccd5f561c010a9cafdedfe5c79a68
Toast Kid <super_q1m@yahoo.co.jp>
2021-01-09 10:41:29 +0900

    refactor Implement FinderAreaView's test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/finder/FinderAreaViewTest.kt
@@ -0,0 +1,37 @@
+package jp.toastkid.yobidashi.compact.editor.finder
+
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
+import kotlinx.coroutines.channels.Channel
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class FinderAreaViewTest {
+
+    private lateinit var finderAreaView: FinderAreaView
+
+    @MockK
+    private lateinit var orderChannel: Channel<FindOrder>
+
+    @MockK
+    private lateinit var messageChannel: Channel<String>
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun test() {
+        finderAreaView = FinderAreaView(orderChannel, messageChannel)
+        assertNotNull(finderAreaView.view())
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+}
