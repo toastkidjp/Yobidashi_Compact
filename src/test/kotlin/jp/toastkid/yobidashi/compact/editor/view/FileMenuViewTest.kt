b01f86579d28597e238bf45d639e4a30ddd72ad8
Toast Kid <super_q1m@yahoo.co.jp>
2021-03-25 07:22:02 +0900

    refactor Implement FileMenuView's unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/view/FileMenuViewTest.kt
@@ -0,0 +1,48 @@
+package jp.toastkid.yobidashi.compact.editor.view
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.yobidashi.compact.editor.MenuCommand
+import kotlinx.coroutines.channels.Channel
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import javax.swing.JMenu
+import javax.swing.JMenuItem
+
+internal class FileMenuViewTest {
+
+    private lateinit var fileMenuView: FileMenuView
+
+    @MockK
+    private lateinit var channel: Channel<MenuCommand>
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        fileMenuView = FileMenuView(channel)
+
+        mockkConstructor(JMenu::class)
+        every { anyConstructed<JMenu>().setMnemonic(any<Char>()) }.answers { mockk() }
+        every { anyConstructed<JMenu>().add(any<JMenuItem>()) }.answers { mockk() }
+    }
+
+    @Test
+    fun test() {
+        fileMenuView.invoke()
+
+        verify(exactly = 1) { anyConstructed<JMenu>().setMnemonic(any<Char>()) }
+        verify(atLeast = 1) { anyConstructed<JMenu>().add(any<JMenuItem>()) }
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+}
