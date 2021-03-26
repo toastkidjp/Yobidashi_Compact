b1deb3b59377f1590e7a9d02e59288018553dee8
Toast Kid <super_q1m@yahoo.co.jp>
2021-03-27 07:28:10 +0900

    refactor Use ChooserPanelFactory.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/calendar/view/CalendarPanel.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/calendar/view/CalendarPanel.kt
@@ -8,77 +8,22 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.Month
 import javax.swing.BoxLayout
-import javax.swing.JButton
 import javax.swing.JFrame
 import javax.swing.JLabel
 import javax.swing.JPanel
-import javax.swing.JSpinner
-import javax.swing.JSpinner.DefaultEditor
-import javax.swing.JTextField
-import javax.swing.SpinnerListModel
-import javax.swing.SpinnerNumberModel
 import javax.swing.border.LineBorder
-import javax.swing.event.ChangeListener
 
 class CalendarPanel : JPanel() {
-
-    private val monthChooser = JSpinner()
-
-    private val yearChooser = JSpinner()
 
     private val dayLabels = Array(6) { arrayOfNulls<JLabel>(7) }
 
     init {
         layout = BoxLayout(this, BoxLayout.Y_AXIS)
-        add(makeChoosersPanel())
+        add(ChooserPanelFactory({ year, month -> refreshDayLabels(year, month) }).invoke())
         add(makeDayPanel())
 
         val date = LocalDate.now()
-        monthChooser.value = date.month
-        yearChooser.value = date.year
-
         refreshDayLabels(date, true)
-    }
-
-    private fun makeChoosersPanel(): JPanel {
-        makeMonthChooser()
-        val monthYearListener: ChangeListener = makeMonthChangeListener()
-        monthChooser.addChangeListener(monthYearListener)
-        makeYearChooser(monthYearListener)
-
-        val chooserPanel = JPanel()
-        chooserPanel.add(JButton("<").also {
-            it.addActionListener {
-                addMonth(-1)
-            }
-        })
-        chooserPanel.add(yearChooser)
-        chooserPanel.add(monthChooser)
-        chooserPanel.add(JButton(">").also {
-            it.addActionListener {
-                addMonth(1)
-            }
-        })
-        return chooserPanel
-    }
-
-    private fun addMonth(additional: Int) {
-        val currentMonth = monthChooser.value as? Month ?: return
-        val next = currentMonth.ordinal + additional
-        val nextMonth = when {
-            next < 0 -> {
-                yearChooser.value = yearChooser.value as Int - 1
-                Month.DECEMBER
-            }
-            next > 11 -> {
-                yearChooser.value = yearChooser.value as Int + 1
-                Month.JANUARY
-            }
-            else -> {
-                Month.values()[next]
-            }
-        }
-        monthChooser.value = nextMonth
     }
 
     private fun makeDayPanel(): JPanel {
@@ -106,34 +51,6 @@ class CalendarPanel : JPanel() {
             }
         }
         return dayPanel
-    }
-
-    private fun makeYearChooser(monthYearListener: ChangeListener) {
-        yearChooser.model = SpinnerNumberModel(2012, 0, 5000, 1)
-        val editor = JSpinner.NumberEditor(yearChooser, "#")
-        yearChooser.editor = editor
-        yearChooser.font = yearChooser.font.deriveFont(14f)
-        yearChooser.addChangeListener(monthYearListener)
-        val dy = yearChooser.preferredSize
-        dy.width += 10 //dy.height += 1;
-        yearChooser.preferredSize = dy
-    }
-
-    private fun makeMonthChangeListener(): ChangeListener {
-        return ChangeListener {
-            refreshDayLabels(yearChooser.value as Int,  Month.valueOf(monthChooser.value.toString()))
-        }
-    }
-
-    private fun makeMonthChooser() {
-        monthChooser.model = SpinnerListModel(Month.values())
-        monthChooser.font = monthChooser.font.deriveFont(14f)
-        val tm = (monthChooser.editor as? DefaultEditor)?.textField
-        tm?.horizontalAlignment = JTextField.CENTER
-
-        val dm = monthChooser.preferredSize
-        dm.width += 10
-        monthChooser.preferredSize = dm
     }
 
     private fun refreshDayLabels(year: Int, month: Month) {
