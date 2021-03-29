65744dd4af93ee8d10f80cc52cacf75561cd431a
Toast Kid <super_q1m@yahoo.co.jp>
2021-03-30 07:56:11 +0900

    refactor Implement ColorChooserService's unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/service/ColorChooserServiceTest.kt
@@ -0,0 +1,43 @@
+package jp.toastkid.yobidashi.compact.editor.service
+
+import io.mockk.every
+import io.mockk.mockkConstructor
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.awt.Color
+import javax.swing.JColorChooser
+import javax.swing.JOptionPane
+
+internal class ColorChooserServiceTest {
+
+    private lateinit var colorChooserService: ColorChooserService
+
+    @BeforeEach
+    fun setUp() {
+        colorChooserService = ColorChooserService()
+
+        mockkStatic(JOptionPane::class)
+        every { JOptionPane.showConfirmDialog(any(), any()) }.returns(JOptionPane.OK_OPTION)
+
+        mockkConstructor(JColorChooser::class)
+        every { anyConstructed<JColorChooser>().getColor() }.returns(Color.BLACK)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun invoke() {
+        colorChooserService.invoke()
+
+        verify(exactly = 1) { JOptionPane.showConfirmDialog(any(), any()) }
+        verify(exactly = 1) { anyConstructed<JColorChooser>().getColor() }
+    }
+
+}
