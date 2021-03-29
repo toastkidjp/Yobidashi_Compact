cf7739e935fd185a1ae8ed6a814a9e17f6ee4936
Toast Kid <super_q1m@yahoo.co.jp>
2021-03-30 08:10:40 +0900

    refactor Specify return type explicitly.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/service/ArticlesReaderService.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/service/ArticlesReaderService.kt
@@ -2,11 +2,13 @@ package jp.toastkid.yobidashi.compact.service
 
 import jp.toastkid.yobidashi.compact.model.Setting
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.stream.Stream
 
 class ArticlesReaderService {
 
-    operator fun invoke() =
+    operator fun invoke(): Stream<Path> =
         Files.list(Paths.get(Setting.articleFolder()))
 
 }
