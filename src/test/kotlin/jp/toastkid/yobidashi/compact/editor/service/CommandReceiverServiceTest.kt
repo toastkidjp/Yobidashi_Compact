b7f79acc3c84b264f0fe9001a2453cdec0a96ef8
Toast Kid <super_q1m@yahoo.co.jp>
2021-04-01 07:01:20 +0900

    refactor Add CommandReceiverServiceTest.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/service/CommandReceiverServiceTest.kt
@@ -0,0 +1,69 @@
+package jp.toastkid.yobidashi.compact.editor.service
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
+import jp.toastkid.yobidashi.compact.editor.MenuCommand
+import jp.toastkid.yobidashi.compact.editor.model.Editing
+import jp.toastkid.yobidashi.compact.editor.view.EditorAreaView
+import jp.toastkid.yobidashi.compact.model.Article
+import jp.toastkid.yobidashi.compact.service.UrlOpenerService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class CommandReceiverServiceTest {
+
+    @InjectMockKs
+    private lateinit var commandReceiverService: CommandReceiverService
+
+    @MockK
+    private lateinit var channel: Channel<MenuCommand>
+
+    @MockK
+    private lateinit var editorAreaView: EditorAreaView
+
+    @MockK
+    private lateinit var currentArticle: () -> Article
+
+    @MockK
+    private lateinit var editing: Editing
+
+    @MockK
+    private lateinit var resetFrameTitle: () -> Unit
+
+    @MockK
+    private lateinit var switchFinder: () -> Unit
+
+    @MockK
+    private lateinit var close: () -> Unit
+
+    @MockK
+    private lateinit var urlOpenerService: UrlOpenerService
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+        every { urlOpenerService.invoke(any<String>()) }.answers { Unit }
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun invoke() {
+        CoroutineScope(Dispatchers.Unconfined).launch {
+            commandReceiverService.invoke()
+        }
+        Unit
+    }
+
+}
