c02546968254643ed49b24255c8b51ee4113289c
Toast Kid <super_q1m@yahoo.co.jp>
2021-04-04 06:45:21 +0900

    refactor Implement YearChooserFactory's unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/calendar/view/YearChooserFactoryTest.kt
@@ -0,0 +1,52 @@
+package jp.toastkid.yobidashi.compact.calendar.view
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.awt.Dimension
+import java.awt.Font
+import javax.swing.JSpinner
+
+internal class YearChooserFactoryTest {
+
+    private lateinit var yearChooserFactory: YearChooserFactory
+
+    @BeforeEach
+    fun setUp() {
+        yearChooserFactory = YearChooserFactory()
+
+        mockkConstructor(JSpinner::class)
+        every { anyConstructed<JSpinner>().setModel(any()) }.answers { Unit }
+        every { anyConstructed<JSpinner>().setFont(any()) }.answers { Unit }
+        val font = mockk<Font>()
+        every { font.deriveFont(any<Float>()) }.returns(font)
+        every { anyConstructed<JSpinner>().getFont() }.returns(font)
+        every { anyConstructed<JSpinner>().getEditor() }.returns(mockk())
+        every { anyConstructed<JSpinner>().getPreferredSize() }.returns(Dimension())
+        every { anyConstructed<JSpinner>().setPreferredSize(any()) }.answers { Unit }
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun invoke() {
+        yearChooserFactory.invoke()
+
+        verify(exactly = 1) { anyConstructed<JSpinner>().setModel(any()) }
+        verify(exactly = 1) { anyConstructed<JSpinner>().setFont(any()) }
+        // Following 2 verifications make crash.
+        //verify(exactly = 1) { anyConstructed<JSpinner>().getFont() }
+        //verify(exactly = 1) { anyConstructed<JSpinner>().getEditor() }
+        verify(exactly = 1) { anyConstructed<JSpinner>().getPreferredSize() }
+        verify(exactly = 1) { anyConstructed<JSpinner>().setPreferredSize(any()) }
+    }
+
+}
