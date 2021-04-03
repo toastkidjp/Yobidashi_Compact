f8a7a97c2cb1c6a23d8b38a96c9726ec14ef8893
Toast Kid <super_q1m@yahoo.co.jp>
2021-04-04 06:43:31 +0900

    refactor Implement DayPanelFactory's unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/calendar/view/DayPanelFactoryTest.kt
@@ -0,0 +1,53 @@
+package jp.toastkid.yobidashi.compact.calendar.view
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import javax.swing.JComponent
+import javax.swing.JPanel
+
+internal class DayPanelFactoryTest {
+
+    @InjectMockKs
+    private lateinit var dayPanelFactory: DayPanelFactory
+
+    @MockK
+    private lateinit var dayOfWeekLabelFactory: DayOfWeekLabelFactory
+
+    @MockK
+    private lateinit var dayLabelFactory: DayLabelFactory
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        every { dayOfWeekLabelFactory.invoke(any()) }.returns(mockk())
+        every { dayLabelFactory.invoke(any()) }.returns(mockk())
+
+        mockkConstructor(JPanel::class)
+        every { anyConstructed<JPanel>().add(any<JComponent>()) }.returns(mockk())
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun invoke() {
+        dayPanelFactory.invoke()
+
+        verify(atLeast = 1) { dayOfWeekLabelFactory.invoke(any()) }
+        verify(atLeast = 1) { dayLabelFactory.invoke(any()) }
+        verify(atLeast = 1) { anyConstructed<JPanel>().add(any<JComponent>()) }
+    }
+
+}
