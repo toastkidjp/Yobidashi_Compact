6e6fec1cf9e702e01c7959ee85120cb46ca881f9
Toast Kid <super_q1m@yahoo.co.jp>
2021-04-06 07:17:08 +0900

    refactor Implement CalendarPanel's unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/calendar/view/CalendarPanelTest.kt
@@ -0,0 +1,38 @@
+package jp.toastkid.yobidashi.compact.calendar.view
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import jp.toastkid.yobidashi.compact.calendar.service.DayLabelRefresherService
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import javax.swing.JLabel
+import javax.swing.JPanel
+
+internal class CalendarPanelTest {
+
+    @BeforeEach
+    fun setUp() {
+        mockkConstructor(DayPanelFactory::class)
+        every { anyConstructed<DayPanelFactory>().invoke() }
+            .returns(JPanel() to Array(6) { arrayOfNulls<JLabel>(7) })
+        mockkConstructor(DayLabelRefresherService::class)
+        every { anyConstructed<DayLabelRefresherService>().invoke(any<Int>(), any()) }.returns(mockk())
+        every { anyConstructed<DayLabelRefresherService>().invoke(any<LocalDate>(), any()) }.returns(mockk())
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun test() {
+        assertNotNull(CalendarPanel())
+    }
+
+}
