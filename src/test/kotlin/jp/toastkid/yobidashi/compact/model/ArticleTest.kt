3e92a60813999f8a6d70600e81bf8b7a1388b8ec
Toast Kid <super_q1m@yahoo.co.jp>
2021-04-08 07:33:09 +0900

    refactor Implement Article's unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/model/ArticleTest.kt
@@ -0,0 +1,46 @@
+package jp.toastkid.yobidashi.compact.model
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
+
+internal class ArticleTest {
+
+    @MockK
+    private lateinit var path: Path
+
+    @MockK
+    private lateinit var file: File
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+        every { path.toFile() }.returns(file)
+        every { file.getName() }.returns("test.md")
+
+        mockkStatic(Paths::class)
+        every { Paths.get(any(), any()) }.returns(path)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun makeWith() {
+        Article.withTitle("test.md")
+
+        verify(exactly = 1) { Paths.get(any(), any()) }
+    }
+
+}
