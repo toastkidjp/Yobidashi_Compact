bee3f90ec69d1b2eb0ce7fd10796e0c9b38e90e2
Toast Kid <super_q1m@yahoo.co.jp>
2021-04-08 07:18:28 +0900

    refactor Rename to testReturnNull.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/editor/service/ClipboardFetcherTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/service/ClipboardFetcherTest.kt
@@ -34,7 +34,7 @@ internal class ClipboardFetcherTest {
     }
 
     @Test
-    fun testNull() {
+    fun testReturnNull() {
         every { transferable.isDataFlavorSupported(any()) }.returns(false)
 
         assertNull(clipboardFetcher.invoke())
