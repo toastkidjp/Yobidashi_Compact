9297cb77d87da65c5083aa63e32055ec3aa2dbc6
Toast Kid <super_q1m@yahoo.co.jp>
2021-06-27 10:23:09 +0900

    refactor Implement OutgoAggregationResult's unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/aggregation/model/OutgoAggregationResultTest.kt
@@ -0,0 +1,27 @@
+package jp.toastkid.yobidashi.compact.aggregation.model
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class OutgoAggregationResultTest {
+
+    private lateinit var outgoAggregationResult: OutgoAggregationResult
+
+    @BeforeEach
+    fun setUp() {
+        outgoAggregationResult = OutgoAggregationResult("target")
+    }
+
+    @Test
+    fun test() {
+        outgoAggregationResult.add("2021-04-30", "Orange", 100)
+        outgoAggregationResult.add("2021-04-30", "Ramen", 211)
+
+        assertEquals(2, outgoAggregationResult.itemArrays().size)
+        assertFalse(outgoAggregationResult.isEmpty())
+        assertEquals("Total: 311", outgoAggregationResult.resultTitleSuffix())
+    }
+
+}
