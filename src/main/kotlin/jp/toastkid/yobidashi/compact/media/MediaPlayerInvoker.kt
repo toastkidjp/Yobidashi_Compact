1e0112b8f26984276793885c07bd8c8831680df1
Toast Kid <super_q1m@yahoo.co.jp>
2022-11-28 08:32:19 +0900

    feature Wrap try-catch.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/media/MediaPlayerInvoker.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/media/MediaPlayerInvoker.kt
@@ -1,18 +1,23 @@
 package jp.toastkid.yobidashi.compact.media
 
 import jp.toastkid.yobidashi.compact.model.Setting
+import java.io.IOException
 import java.nio.file.Path
 import kotlin.io.path.absolutePathString
 
 class MediaPlayerInvoker {
 
     operator fun invoke(mediaFilePath: Path) {
-        Runtime.getRuntime().exec(
-                arrayOf(
-                        Setting.mediaPlayerPath(),
-                        mediaFilePath.absolutePathString()
-                )
-        )
+        try {
+            Runtime.getRuntime().exec(
+                    arrayOf(
+                            Setting.mediaPlayerPath(),
+                            mediaFilePath.absolutePathString()
+                    )
+            )
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 
 }
