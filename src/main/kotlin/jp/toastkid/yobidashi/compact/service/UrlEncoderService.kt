8cbd2f83acf08589bac4800f917ee33d26efa77e
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-01 09:24:55 +0900

    refactor Rename local variable.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/service/UrlEncoderService.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/service/UrlEncoderService.kt
@@ -17,25 +17,25 @@ class UrlEncoderService {
     operator fun invoke() {
         val panel = JPanel()
         panel.layout = BoxLayout(panel, BoxLayout.PAGE_AXIS)
-        val unixTimeInput = JTextField()
-        unixTimeInput.text = "東京特許 許可局"
+        val rawWordInput = JTextField()
+        rawWordInput.text = "東京特許 許可局"
         val dateTime = JTextField()
-        dateTime.text = URLEncoder.encode(unixTimeInput.text, StandardCharsets.UTF_8.name())
+        dateTime.text = URLEncoder.encode(rawWordInput.text, StandardCharsets.UTF_8.name())
 
-        unixTimeInput.preferredSize = Dimension(100, 24)
-        unixTimeInput.addKeyListener(object : KeyAdapter() {
+        rawWordInput.preferredSize = Dimension(100, 24)
+        rawWordInput.addKeyListener(object : KeyAdapter() {
             override fun keyReleased(e: KeyEvent?) {
-                dateTime.text = URLEncoder.encode(unixTimeInput.text, StandardCharsets.UTF_8.name())
+                dateTime.text = URLEncoder.encode(rawWordInput.text, StandardCharsets.UTF_8.name())
             }
         })
         panel.add(JLabel("Please would you input some words."))
         panel.add(JLabel("Raw string"))
-        panel.add(unixTimeInput)
+        panel.add(rawWordInput)
         panel.add(JLabel("Encoded string"))
         dateTime.preferredSize = Dimension(100, 24)
         dateTime.addKeyListener(object : KeyAdapter() {
             override fun keyReleased(e: KeyEvent?) {
-                unixTimeInput.text = URLDecoder.decode(dateTime.text, StandardCharsets.UTF_8.name())
+                rawWordInput.text = URLDecoder.decode(dateTime.text, StandardCharsets.UTF_8.name())
             }
         })
         panel.add(dateTime)
