0beab3a7a325580d6c1887b8b7edc2b44bb9a295
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-01 09:26:17 +0900

    refactor Move codes.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/service/UrlEncoderService.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/service/UrlEncoderService.kt
@@ -26,18 +26,18 @@ class UrlEncoderService {
                 encodedInput.text = URLEncoder.encode(rawWordInput.text, StandardCharsets.UTF_8.name())
             }
         })
-        val panel = JPanel()
-        panel.layout = BoxLayout(panel, BoxLayout.PAGE_AXIS)
-        panel.add(JLabel("Please would you input some words."))
-        panel.add(JLabel("Raw string"))
-        panel.add(rawWordInput)
-        panel.add(JLabel("Encoded string"))
         encodedInput.preferredSize = Dimension(100, 24)
         encodedInput.addKeyListener(object : KeyAdapter() {
             override fun keyReleased(e: KeyEvent?) {
                 rawWordInput.text = URLDecoder.decode(encodedInput.text, StandardCharsets.UTF_8.name())
             }
         })
+        val panel = JPanel()
+        panel.layout = BoxLayout(panel, BoxLayout.PAGE_AXIS)
+        panel.add(JLabel("Please would you input some words."))
+        panel.add(JLabel("Raw string"))
+        panel.add(rawWordInput)
+        panel.add(JLabel("Encoded string"))
         panel.add(encodedInput)
         JOptionPane.showMessageDialog(null, panel)
     }
