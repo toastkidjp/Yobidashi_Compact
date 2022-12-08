e6dbb102d90afd2fff75e405ecb6db88b293fae5
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-08 09:27:08 +0900

    feature Add web-search menu.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/editor/view/EditorToolMenuView.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/editor/view/EditorToolMenuView.kt
@@ -1,14 +1,27 @@
 package jp.toastkid.yobidashi.compact.editor.view
 
 import jp.toastkid.yobidashi.compact.editor.MenuCommand
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
 import javax.swing.JMenu
+import javax.swing.JMenuItem
 
 class EditorToolMenuView(private val channel: Channel<MenuCommand>) {
 
     operator fun invoke(): JMenu {
         val menu = JMenu("Tool(T)")
         menu.setMnemonic('T')
+
+        val webSearchMenu = JMenuItem("Web search").also {
+            it.addActionListener {
+                CoroutineScope(Dispatchers.Default).launch {
+                    channel.send(MenuCommand.WEB_SEARCH)
+                }
+            }
+        }
+        menu.add(webSearchMenu)
 
         return menu
     }
