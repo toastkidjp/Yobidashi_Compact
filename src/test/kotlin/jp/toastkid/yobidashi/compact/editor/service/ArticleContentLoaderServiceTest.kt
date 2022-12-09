d033d26d96a3d3ded8241f3e3f31f3a2e4246fb9
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-10 08:40:23 +0900

    refactor Modify test.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/editor/service/ArticleContentLoaderServiceTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/service/ArticleContentLoaderServiceTest.kt
@@ -1,30 +1,25 @@
 package jp.toastkid.yobidashi.compact.editor.service
 
-import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
-import jp.toastkid.yobidashi.compact.model.Article
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.nio.file.Files
+import java.nio.file.Path
 
 internal class ArticleContentLoaderServiceTest {
 
     @MockK
-    private lateinit var article: Article
+    private lateinit var article: Path
 
     @BeforeEach
     fun setUp() {
         mockkStatic(Files::class)
         every { Files.readAllLines(any()) }.answers { listOf("line1", "line2") }
-
-        MockKAnnotations.init(this)
-        every { article.path() }.answers { mockk() }
     }
 
     @Test
