6c0e719f3dead161004593f8b9170032fdb76ac6
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-10 08:42:25 +0900

    feature Enable to edit file when this app is passed file-path.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/Main.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/Main.kt
@@ -1,13 +1,22 @@
 package jp.toastkid.yobidashi.compact
 
+import jp.toastkid.yobidashi.compact.editor.EditorFrame
 import jp.toastkid.yobidashi.compact.model.Setting
 import jp.toastkid.yobidashi.compact.view.MainFrame
+import java.nio.file.Paths
 
 /**
  * Created by toastkidjp on 2020/03/23.
  * @author toastkidjp
  */
-fun main() {
+fun main(array: Array<String>?) {
+    if (array?.isNotEmpty() == true) {
+        val path = Paths.get(array[0]) ?: return
+        val editorFrame = EditorFrame()
+        editorFrame.load(path)
+        editorFrame.show()
+        return
+    }
     val frame = MainFrame("Yobidashi Compact")
     frame.show()
     Runtime.getRuntime().addShutdownHook(Thread {
