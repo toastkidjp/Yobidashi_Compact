7aa2cce9888ca2258847906973672c971eebf0d5
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-14 08:21:34 +0900

    refactor Divide to other class.

---
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/service/CommandLineArgumentService.kt
@@ -0,0 +1,18 @@
+package jp.toastkid.yobidashi.compact.service
+
+import jp.toastkid.yobidashi.compact.editor.EditorFrame
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class CommandLineArgumentService {
+
+    operator fun invoke(array: Array<String>) {
+        array.mapNotNull { Paths.get(it) }
+            .filter { Files.isReadable(it) }
+            .forEach {
+                val editorFrame = EditorFrame()
+                editorFrame.load(it)
+                editorFrame.show()
+            }
+    }
+}
