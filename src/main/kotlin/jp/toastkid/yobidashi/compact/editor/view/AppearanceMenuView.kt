cbf9e412ff50084b4b316beaa32cf53075b3544c
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-14 08:23:42 +0900

    refactor Use KeyEvent.CTRL_DOWN_MASK.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/editor/view/AppearanceMenuView.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/editor/view/AppearanceMenuView.kt
@@ -27,7 +27,7 @@ class AppearanceMenuView(private val channel: Channel<MenuCommand>) {
         menu.add(findItem)
 
         val wrapLineSwitchItem = JCheckBoxMenuItem("Wrap line", Setting.wrapLine())
-        wrapLineSwitchItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_W, Event.SHIFT_MASK or Event.CTRL_MASK)
+        wrapLineSwitchItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_W, Event.SHIFT_MASK or KeyEvent.CTRL_DOWN_MASK)
         wrapLineSwitchItem.addActionListener {
             CoroutineScope(Dispatchers.IO).launch {
                 channel.send(MenuCommand.SWITCH_WRAP_LINE)
