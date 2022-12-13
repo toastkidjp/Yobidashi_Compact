8cefda9b5a4c2ee0957a65402cd53c3af179c352
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-14 08:22:18 +0900

    refactor Use divided class.

--- src/main/kotlin/jp/toastkid/yobidashi/compact/Main.kt
+++ src/main/kotlin/jp/toastkid/yobidashi/compact/Main.kt
@@ -1,10 +1,8 @@
 package jp.toastkid.yobidashi.compact
 
-import jp.toastkid.yobidashi.compact.editor.EditorFrame
 import jp.toastkid.yobidashi.compact.model.Setting
+import jp.toastkid.yobidashi.compact.service.CommandLineArgumentService
 import jp.toastkid.yobidashi.compact.view.MainFrame
-import java.nio.file.Files
-import java.nio.file.Paths
 
 /**
  * Created by toastkidjp on 2020/03/23.
@@ -12,7 +10,7 @@ import java.nio.file.Paths
  */
 fun main(array: Array<String>?) {
     if (array?.isNotEmpty() == true) {
-        openFilesWithEditor(array)
+        CommandLineArgumentService().invoke(array)
         return
     }
     val frame = MainFrame("Yobidashi Compact")
@@ -20,14 +18,4 @@ fun main(array: Array<String>?) {
     Runtime.getRuntime().addShutdownHook(Thread {
         Setting.save()
     })
-}
-
-private fun openFilesWithEditor(array: Array<String>) {
-    array.mapNotNull { Paths.get(it) }
-        .filter { Files.isReadable(it) }
-        .forEach {
-            val editorFrame = EditorFrame()
-            editorFrame.load(it)
-            editorFrame.show()
-        }
 }
