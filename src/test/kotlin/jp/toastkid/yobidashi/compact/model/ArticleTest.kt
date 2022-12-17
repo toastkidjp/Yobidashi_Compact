23b59c85d5943fd1850eb904dd2e5dfccaf86d05
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-17 11:42:37 +0900

    refactor Rename test case.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/model/ArticleTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/model/ArticleTest.kt
@@ -138,7 +138,7 @@ internal class ArticleTest {
     }
 
     @Test
-    fun lastModifiedThrowingIoException() {
+    fun testLastModifiedThrowingIoExceptionFromFiles() {
         val fileTime = mockk<FileTime>()
         every { fileTime.toMillis() }.returns(42L)
         mockkStatic(Files::class)
