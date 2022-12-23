ba2609d9795bb943eadf5b2952d2997d8b329609
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-24 08:35:47 +0900

    refactor Use property access syntax.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/editor/EditorFrameTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/EditorFrameTest.kt
@@ -46,7 +46,7 @@ internal class EditorFrameTest {
 
         val font = mockk<Font>()
         every { font.deriveFont(any<Float>()) }.returns(font)
-        every { statusLabel.getFont() }.returns(font)
+        every { statusLabel.font }.returns(font)
         every { statusLabel.setFont(any()) }.just(Runs)
 
         every { editing.setCurrentSize(any()) }.just(Runs)
