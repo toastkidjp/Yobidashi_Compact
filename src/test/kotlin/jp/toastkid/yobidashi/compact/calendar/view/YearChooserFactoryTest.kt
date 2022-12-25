b3904446522acf285139fd3e4475020af3d2f191
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-25 10:16:17 +0900

    refactor Use property access syntax.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/calendar/view/YearChooserFactoryTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/calendar/view/YearChooserFactoryTest.kt
@@ -23,7 +23,7 @@ internal class YearChooserFactoryTest {
         yearChooserFactory = YearChooserFactory()
 
         mockkConstructor(JSpinner::class)
-        every { anyConstructed<JSpinner>().setModel(any()) }.just(Runs)
+        every { anyConstructed<JSpinner>().model = any() }.just(Runs)
         every { anyConstructed<JSpinner>().setFont(any()) }.just(Runs)
         val font = mockk<Font>()
         every { font.deriveFont(any<Float>()) }.returns(font)
