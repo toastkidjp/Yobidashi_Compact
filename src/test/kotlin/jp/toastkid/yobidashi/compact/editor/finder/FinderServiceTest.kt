afb3ef29b98d02816a238f558a6b9970ef2dca78
Toast Kid <super_q1m@yahoo.co.jp>
2022-12-29 08:50:48 +0900

    refactor Use property access syntax.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/editor/finder/FinderServiceTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/finder/FinderServiceTest.kt
@@ -83,7 +83,7 @@ internal class FinderServiceTest {
 
     @Test
     fun testNotFoundCase() {
-        every { editorArea.getText() }.returns("ACB news will go bankrupt ACB.")
+        every { editorArea.text }.returns("ACB news will go bankrupt ACB.")
         every { editorArea.replaceRange(any(), any(), any()) }.answers { Unit }
 
         finderService.invoke(FindOrder("axx", "BXX", invokeReplace = true, caseSensitive = false))
