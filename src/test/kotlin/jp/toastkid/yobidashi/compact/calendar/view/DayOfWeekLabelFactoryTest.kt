2b12a6212e1bdc6a16ca59a57fdf3b7587a3836e
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-01 09:15:09 +0900

    refactor Use property access syntax.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/calendar/view/DayOfWeekLabelFactoryTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/calendar/view/DayOfWeekLabelFactoryTest.kt
@@ -50,7 +50,7 @@ internal class DayOfWeekLabelFactoryTest {
         verify (exactly = 1) { anyConstructed<JLabel>().horizontalAlignment = any() }
         verify (exactly = 1) { anyConstructed<JLabel>().verticalAlignment = any() }
         verify (exactly = 1) { anyConstructed<JLabel>().preferredSize = any() }
-        verify (exactly = 1) { anyConstructed<JLabel>().setOpaque(any()) }
+        verify (exactly = 1) { anyConstructed<JLabel>().isOpaque = any() }
         verify (exactly = 1) { anyConstructed<JLabel>().setBorder(any()) }
         verify (exactly = 1) { anyConstructed<JLabel>().setBackground(any()) }
         verify (exactly = 0) { anyConstructed<JLabel>().setForeground(any()) }
