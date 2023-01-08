97a0c0798d58fe99a3dadd860484dbda675b98e7
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-08 09:30:19 +0900

    refactor Use property access syntax.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/editor/view/EditorAreaViewTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/view/EditorAreaViewTest.kt
@@ -149,7 +149,7 @@ internal class EditorAreaViewTest {
     @Test
     fun testReplaceSelected() {
         every { editorArea.selectedText }.returns("test")
-        every { editorArea.getSelectionStart() }.returns(2)
+        every { editorArea.selectionStart }.returns(2)
         every { editorArea.getSelectionEnd() }.returns(3)
         every { editorArea.replaceSelection(any()) }.answers { Unit }
 
