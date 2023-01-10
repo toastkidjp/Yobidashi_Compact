3d86aab655b7c86e15a2a248d8c2b872feb0c2c2
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-11 08:33:13 +0900

    refactor Use just runs.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/editor/view/EditorAreaViewTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/view/EditorAreaViewTest.kt
@@ -138,7 +138,7 @@ internal class EditorAreaViewTest {
         every { editorArea.selectedText }.returns(null)
         every { editorArea.selectionStart }.returns(2)
         every { editorArea.selectionEnd }.returns(3)
-        every { editorArea.replaceSelection(any()) }.answers { Unit }
+        every { editorArea.replaceSelection(any()) }.just(Runs)
 
         editorAreaView.replaceSelected(false, { "replaced" })
 
