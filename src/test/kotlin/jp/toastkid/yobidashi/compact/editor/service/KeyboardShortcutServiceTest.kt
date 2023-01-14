613a43dc190e40c82cb0ffe5ce034b86eca0856b
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-14 09:57:18 +0900

    refactor Use returns.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/editor/service/KeyboardShortcutServiceTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/service/KeyboardShortcutServiceTest.kt
@@ -52,7 +52,7 @@ internal class KeyboardShortcutServiceTest {
 
     @Test
     fun testIsControlAndShiftDown() {
-        every { keyEvent.isControlDown() }.answers { true }
+        every { keyEvent.isControlDown() }.returns(true)
         every { keyEvent.isShiftDown() }.answers { true }
         every { keyEvent.getKeyCode() }.answers { KeyEvent.VK_U }
 
