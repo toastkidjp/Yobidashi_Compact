3820a55583967651d8f10eba0c68b44485eb0616
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-18 19:04:10 +0900

    refactor Use property access syntax.

--- src/test/kotlin/jp/toastkid/yobidashi/compact/editor/service/KeyboardShortcutServiceTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi/compact/editor/service/KeyboardShortcutServiceTest.kt
@@ -60,7 +60,7 @@ internal class KeyboardShortcutServiceTest {
 
         verify(exactly = 1) { keyEvent.isControlDown }
         verify(exactly = 1) { keyEvent.isShiftDown }
-        verify(exactly = 1) { keyEvent.getKeyCode() }
+        verify(exactly = 1) { keyEvent.keyCode }
         coVerify(exactly = 1) { channel.send(any()) }
     }
 
